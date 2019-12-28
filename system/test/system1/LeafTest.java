eb905455c6d759e4cd04a5fcb796786f23f39c2f
shulamit <shulamit109@walla.com>
2019-12-28 15:57:31 +0200

    init

--- system/test/system1/LeafTest.java
+++ system/test/system1/LeafTest.java
@@ -1,14 +1,22 @@
 package system1;
 
+import org.junit.Before;
 import org.junit.Test;
+import system.Leaf;
+import system.OutOfSpaceException;
 
 import static org.junit.Assert.*;
 
 public class LeafTest {
+    Leaf leaf;
 
-    @Test
-    public void checkLeaf(){
-
+    @Before
+    public void checkLeaf () throws OutOfSpaceException {
+    leaf= new Leaf("file", 5);
+    assertNotNull(leaf);
     }
+
+
+
 
 }
--- system/test/system1/SpaceTest.java
+++ system/test/system1/SpaceTest.java
@@ -2,21 +2,53 @@ package system1;
 
 import org.junit.Before;
 import org.junit.Test;
+import system.Leaf;
+import system.OutOfSpaceException;
 import system.Space;
+
+import java.util.LinkedList;
 
 import static org.junit.Assert.*;
 
 public class SpaceTest {
 
     Space space;
+    Leaf[] blocks;
+    LinkedList<Integer> freeBlocks;
+    Leaf file;
+
+
 
     @Before
-    public void initialize(){
+    public void initialize() throws OutOfSpaceException, NullPointerException {
         space = new Space(5);
+        assertNotNull(new Leaf("file", 5));
+    }
+
+
+
+
+    @Test
+    public void checkAlloc() throws OutOfSpaceException {
+
+        space.Alloc(5, file);
+    }
+    //exception?
+    @Test
+    public void checkDealloc(){
+
+        space.Dealloc(file);
+        assertEquals(0,space.countFreeSpace());
     }
 
     @Test
-    public void checkSpace()  {
-        assertEquals(5, space.countFreeSpace());
+    public void checkCountFreeSpace() throws OutOfSpaceException {
+        space.Dealloc(file);
+        space.Alloc(5, file);
+        assertEquals(5,space.countFreeSpace());
+
     }
+
+
+
 }
